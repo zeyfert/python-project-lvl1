e7e0720b1fb1dd82f569af89d7428865bb5c5ef8
Maxim Zeyfert <zeyfert.m@outlook.com>
2020-09-28 13:39:29 +0300

    Global variables 'LENGTH_PROGRESSION' is added in order to avoid values with non clear meaning 'magic numbers'

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,6 +2,7 @@ from random import randint
 
 
 DESCRIPTION = 'What number is missing in the progression?'
+LENGTH_PROGRESSION = 10
 
 
 def get_question_and_result():
@@ -15,6 +16,6 @@ def get_question_and_result():
 
 
 def get_progression_and_index():
-    first_value, step, index = randint(1, 100), randint(1, 10), randint(0, 9)
-    last_value = step * 10 + first_value
+    first_value, step, index = randint(1, 100), randint(1, 10), randint(0, LENGTH_PROGRESSION)
+    last_value = step * LENGTH_PROGRESSION + first_value
     return [str(i) for i in range(first_value, last_value, step)], index
