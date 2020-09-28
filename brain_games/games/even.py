ff8a7865bdb26d45b17fe339f0ce56caf431f002
Maxim Zeyfert <zeyfert.m@outlook.com>
2020-09-28 11:55:14 +0300

    Changed returned value for is_even method. Before it returned str (yes or no). Not it return bool value and after that the bool value should be converted to (yes/no) in the get_question_and_result method

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,12 +5,27 @@ DESCRIPTION = 'Answer "yes" if number even otherwise answer "no".'
 
 
 def get_question_and_result():
+    """ Method provides a random value
+        And result of check "Is it even?"
+        The answer may be "yes" or "no"
+        
+        Returns:
+            dictionary with value and answer
+    """
     value = randint(1, 10)
+    result = 'yes' if is_even(value) else 'no'
     return {
         'question': value,
-        'result': is_even(value)
+        'result': result,
     }
 
 
 def is_even(value):
-    return 'yes' if value % 2 == 0 else 'no'
+    """ Method checks if a value is even or not
+    
+        Parameters:
+            value (int): the value which should be ckecked
+        Returns:
+            result of check (bool): it may be true or false
+    """     
+    return value % 2 == 0
