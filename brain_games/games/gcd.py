3b84073069002ec5459a9911b738d138da6b2afe
Maxim Zeyfert <zeyfert.m@outlook.com>
2020-09-28 12:17:39 +0300

    The name of method for finding greatest common divisor was changed from 'calculate' -> 'get_gcd'

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -8,11 +8,18 @@ def get_question_and_result():
     value_1, value_2 = randint(1, 100), randint(1, 100)
     return {
         'question': '{} {}'.format(value_1, value_2),
-        'result': str(calculate(value_1, value_2))
+        'result': str(get_gcd(value_1, value_2))
     }
 
 
-def calculate(value_1, value_2):
+def get_gcd(value_1, value_2):
+    """ Provides greatest common divisor for 2 values
+
+        Parameters:
+            value_1, value_2 (int): random values
+        Returns:
+            greatest common divisor (int)
+    """
     while value_1 != 0 and value_2 != 0:
         if value_1 > value_2:
             value_1 %= value_2
