ac52c9809a0318ed2e9f3b2d4108d79c520cf05e
Maxim Zeyfert <zeyfert.m@outlook.com>
2020-09-29 19:52:26 +0300

    Logic and returned value are changed. Now it returns tuple

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,14 +6,26 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def get_question_and_result():
     value = randint(1, 3571)
-    return {
-        'question': value,
-        'result': is_prime(value)
-    }
+    result = 'yes' if is_prime(value) else 'no'
+    return (value, result)
 
 
 def is_prime(value):
-    divider = 2
-    while value % divider != 0:
+    """ Checks if the value is prime or not
+
+        Parameters:
+            value (int): the value for check
+
+        Returns:
+            result (bool): True if prime else False
+
+    """
+    divider = 3
+    if value < 2 or value % 2 == 0:
+        return False
+    divider = 3
+    while divider <= value // 2:
+        if value % divider == 0:
+            return False
         divider += 1
-    return 'yes' if value == divider else 'no'
+    return True
