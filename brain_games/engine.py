fec27712407f37fa5f8dbf21c5f78ec932cb7b1a
Maxim Zeyfert <zeyfert.m@outlook.com>
2020-09-29 20:58:12 +0300

    The name of variables 'correct_answers' is changed to 'answers_count'. The type of returned value in the functions 'get_question_and_result' is changed from dictionary to tuple. The condition 'if correct_answers == 3' is changed for 'else' after while section. The name on the main function is changed from start_game to play

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,23 +1,23 @@
 from prompt import string
 
 
-def start_game(game):
+def play(game):
     print('Welcome to the Brain Games!')
     print(game.DESCRIPTION, '\n')
     name = string('May I have your name? ')
     print('Hello, {}!\n'.format(name))
-    correct_answers = 0
-    while correct_answers < 3:
-        question, result = game.get_question_and_result().values()
+    answers_count = 0
+    while answers_count < 3:
+        question, result = game.get_question_and_result()
         print(question)
         user_answer = string('Your answer: ')
         if result != user_answer:
             print("'{}' is wrong answer ;(. Correct answer was '{}'"
                   .format(user_answer, result))
             print("Let's try again, {}".format(name))
-            break
+            return
         else:
             print('Correct!')
-            correct_answers += 1
-    if correct_answers == 3:
+            answers_count += 1
+    else:
         print('Congratulations, {}'.format(name))
